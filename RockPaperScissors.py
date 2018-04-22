d541d20f3a91ef547e99669162c01a8d86568c8b
Kenneth Lo <kennethkclo8@gmail.com>
2018-04-22 10:17:30 -0700

    Checked in rock paper scissors question prompt

---
+++ RockPaperScissors.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 22 00:10:01 2018
+
+Make a two-player rock-paper-scissors games. 
+Hint: 
+    Ask for player plays (using input)
+    Compare answers
+    Print out a message of congratulations to the winner
+    
+Options: 'rock', 'scissors', 'paper'
+
+# Input()
+# Print()
+# String
+# Strin concatenation
+
+@author: Kennethlo
+"""
+
